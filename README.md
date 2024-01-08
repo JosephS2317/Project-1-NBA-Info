c055762c93b5ac0f6a89cba254e172483628b86e
Joseph <JosephSped23@gmail.com>
2024-01-08 14:49:23 -0500

    committing changes to the repo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Project-1-NBA-Info
-Phase 1 project for flatiron school. Single Page application that lets you search NBA team names and get some information about the team.
+## Phase 1 Project: NBA Team Search
+
+NBA Team search is a one page application where the user can type in a team name in the search bar. This triggers a function that filters using (.filter)the team you search for then creates a card using (.forEach(team)). So whatever team matches the filter the createTeamCard() will trigger creating that card.
+
+This can display any info about the team that the API offers. Using two even liseners ('mouseover' and 'mouseleave') I have created a functions that will display differnt info depending on if you mouse is hovering over the card or not.
+
+There are four event listeners ('DOMCONTENTLOADED','click', 'mouseover', 'mouseleave') that are triggering all the functionality of the page based on the users mouse interacting with the page.
+
+There are using a db.json to pull our data from in order to give the pages functions content. The page connects the 3 pillars in order to accomplish the end goal for the user. Type a nba teams name into the search bar, and get a card that displays the information.
---
+++ index.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchButton = document.getElementById('searchButton')
+    searchButton.addEventListener('click', searchTeams)
+});
+  
+  const teamSearch = document.getElementsByClassName('search-team')
+  const teamDataUrl = "http://localhost:3000/data"
+  const currentTeams = document.getElementById('current-teams')
+  const teamInput = document.getElementById('searchInput')
+  
+  function searchTeams() {
+    fetch(teamDataUrl)
+      .then((res) => res.json())
+      .then((teams) => {
+        const foundTeams = teams.filter((team) => team.name.toLowerCase()===(teamInput.value.toLowerCase()))
+        foundTeams.forEach((team) => {
+          createTeamCard(team)
+        })
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+function createTeamCard(team) {
+    
+    const card = document.createElement('div')
+    card.className = 'card'
+    
+    currentTeams.append(card)
+   
+    
+  
+    const name = document.createElement('h1')
+    name.textContent = team.name
+  
+    const logo = document.createElement('img')
+    logo.className = 'logo'
+    logo.src = team.logo
+  
+    const city = document.createElement('h2')
+    city.textContent = team.city
+    card.append(city, name, logo)
+    const originalCard = card.innerHTML
+
+
+    
+  
+
+  card.addEventListener('mouseover', function(){
+    fetch(teamDataUrl)
+      .then((res) => res.json())
+      .then((teams) => {
+        card.innerHTML = `
+        <h2>${team.full_name}</h2>
+        <h2>Division: ${team.division}</h2>
+        <h2>Conference: ${team.conference}</h2>
+        `
+      })
+    })
+  card.addEventListener('mouseleave', function() {
+    card.innerHTML = originalCard
+  })
+}
