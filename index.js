d460dc76cf6de66ded3c38e74c84520f1a07f591
Joseph <JosephSped23@gmail.com>
2024-01-25 16:15:19 -0500

    added delete button

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('searchButton')
     searchButton.addEventListener('click', searchTeams)
-});
+})
   
   const teamSearch = document.getElementsByClassName('search-team')
   const teamDataUrl = "http://localhost:3000/data"
   const currentTeams = document.getElementById('current-teams')
   const teamInput = document.getElementById('searchInput')
+  const cardDelete = document.createElement('button')
+  cardDelete.textContent = 'Clear Teams'
+  cardDelete.className = 'delete'
   
   function searchTeams() {
     fetch(teamDataUrl)
@@ -20,20 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
       .catch((error) => {
         console.error(error)
       })
-
-      
   }
-function createTeamCard(team) {
-    
+
+  function createTeamCard(team) {
     const card = document.createElement('div')
     card.className = 'card'
-    
-    currentTeams.append(card)
-   
-    
   
     const name = document.createElement('h1')
-    name.textContent = team.name
+    name.textContent = team.name;
   
     const logo = document.createElement('img')
     logo.className = 'logo'
@@ -41,25 +38,73 @@ function createTeamCard(team) {
   
     const city = document.createElement('h2')
     city.textContent = team.city
+  
+   
     card.append(city, name, logo)
     const originalCard = card.innerHTML
-
-
+  
+    card.addEventListener('mouseover', function() {
+      fetch(teamDataUrl)
+        .then((res) => res.json())
+        .then((teams) => {
+          card.innerHTML = `
+            <h2>${team.full_name}</h2>
+            <h2>Division: ${team.division}</h2>
+            <h2>Conference: ${team.conference}</h2>
+          `
+        })
+    })
+  
+    card.addEventListener('mouseleave', function() {
+      card.innerHTML = originalCard
+    })
+  
+    cardDelete.addEventListener('click', () => {
+      currentTeams.innerHTML = ''
+    })
+  
+    currentTeams.append(card, cardDelete)
+  }
+// function createTeamCard(team) {
+    
+//     currentTeams.append(card)
+   
     
   
+//     const name = document.createElement('h1')
+//     name.textContent = team.name
+  
+//     const logo = document.createElement('img')
+//     logo.className = 'logo'
+//     logo.src = team.logo
+  
+//     const city = document.createElement('h2')
+//     city.textContent = team.city
 
-  card.addEventListener('mouseover', function(){
-    fetch(teamDataUrl)
-      .then((res) => res.json())
-      .then((teams) => {
-        card.innerHTML = `
-        <h2>${team.full_name}</h2>
-        <h2>Division: ${team.division}</h2>
-        <h2>Conference: ${team.conference}</h2>
-        `
-      })
-    })
-  card.addEventListener('mouseleave', function() {
-    card.innerHTML = originalCard
-  })
-}
+//   const card = document.createElement('div')
+//   card.className = 'card'
+//   const originalCard = card.innerHTML
+
+   
+//     card.append(city, name, logo)
+//     currentTeams.append(card)
+
+//     card.addEventListener('mouseover', function(){
+//     fetch(teamDataUrl)
+//       .then((res) => res.json())
+//       .then((team) => {
+//         card.innerHTML = `
+//         <h2>${team.full_name}</h2>
+//         <h2>Division: ${team.division}</h2>
+//         <h2>Conference: ${team.conference}</h2>
+//         `
+//       })
+//   })
+
+//   card.addEventListener('mouseleave', function() {
+//     card.innerHTML = originalCard
+//   })
+// }
+//   cardDelete.addEventListener('click', ()=> {
+//     currentTeams.innerHTML = ''
+//   })
